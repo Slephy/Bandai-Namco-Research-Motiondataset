40c37763040291f784f36be4bb59306963b3dd6d
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-04-27 02:01:51 +0900

    Fix context handling in save function

--- utils/blender/convert_bvh2mp4.py
+++ utils/blender/convert_bvh2mp4.py
@@ -63,8 +63,9 @@ def save(path, frame_num, viewpoint='FRONT'):
             ctx = bpy.context.copy()
             ctx['area'] = area
             ctx['region'] = area.regions[-1]
-            bpy.ops.view3d.view_selected(ctx)
-            bpy.ops.view3d.view_axis(ctx, type=viewpoint)
+            with bpy.context.temp_override(**ctx):
+                bpy.ops.view3d.view_selected()
+                bpy.ops.view3d.view_axis(type=viewpoint)
 
     # Set resolution.
     bpy.context.scene.render.resolution_x = 640
